951829139318fa37e2463b1c4fb2be26a919b1da
Long Mann <layfon@live.com.au>
2022-06-09 22:34:58 +1000

    features added: ability to toggle read/not read take user input from pop-up form instead of prompts. added css to make it look better

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 let myLibrary = [];
 const displayArea = document.querySelector(".book-display-container");
-const addBookButton = document.querySelector(".add"+"Book");
+const addBookButton = document.querySelector(".addBook");
+const addBookButton1 = document.querySelector(".submit-book")
 
 
 function Book(title, author, pages, doneReading) {
@@ -12,22 +13,24 @@ function Book(title, author, pages, doneReading) {
 }
 
 function addBookToLibrary() {
-  let title = prompt("Title of Book:");
-  let author = prompt("Author of Book:");
-  let pages = prompt("number of pages:");
-  let doneReading = prompt("reading status(done/not done):");
-
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pageNum").value;
+  let doneReading = document.getElementById("readStatus").checked;
+  console.log(doneReading);
   const book = new Book(title, author, pages, doneReading);
   myLibrary.push(book);
 
   updateLibrary();
+  document.querySelector(".form-popup").style.display = "none";
 }
 
 function updateLibrary() {
+  //wipe all displayed books
   while(displayArea.firstChild){
     displayArea.removeChild(displayArea.firstChild);
   }
-
+//re-filter array for only non-undefined elements to be displayed
   for(let i = 0; i < myLibrary.length; i++) {
      if(myLibrary[i] !== undefined){
        displayBook(i);
@@ -35,22 +38,56 @@ function updateLibrary() {
   }
 }
 
+function updateReadStatus(e) {
+  let bookId = e.target.id;
+  let num = bookId.match(/\d/g);
+  num = num.join("");
+
+  myLibrary[num].doneReading = !myLibrary[num].doneReading;
+  updateLibrary();
+}
+
+//displayBook function creates elements using information stored in myLibrary
 function displayBook(n) {
   const createDiv = document.createElement("div");
-  const createBtn = document.createElement("button");
-  createBtn.setAttribute("class", "delete-book");
-  createBtn.setAttribute("id", "book"+n);
-  createBtn.textContent = "Delete Book";
+  const createDeleteBtn = document.createElement("button");
+  const createReadBtn = document.createElement("button");
+  const createP1 = document.createElement("p");
+  const createP2 = document.createElement("p");
+  const createP3 = document.createElement("p");
   createDiv.setAttribute("id", "div"+n);
   displayArea.appendChild(createDiv);
 
   const bookCard = document.querySelector("#div" + n);
+  bookCard.setAttribute("data-type", "title");
   bookCard.textContent = myLibrary[n].title;
-  createBtn.addEventListener("click", deleteBook);
-  bookCard.appendChild(createBtn);
+  createP1.innerText = "Author: " + myLibrary[n].author;
+  createP2.innerText = "Number of pages: " + myLibrary[n].pages;
+  createP3.innerText = "Reading Status: " + myLibrary[n].doneReading;
+  bookCard.appendChild(createP1);
+  bookCard.appendChild(createP2);
+  bookCard.appendChild(createP3);
+  createReadBtn.setAttribute("class", "bookcard-btn");
+  createReadBtn.setAttribute("id", "read"+n);
+
+  if(myLibrary[n].doneReading){
+    createReadBtn.classList.add("bookcard-btn-green");
+    createReadBtn.textContent = "Done Reading";
+  }else{
+    createReadBtn.classList.add("bookcard-btn-red");
+    createReadBtn.textContent = "Not done Reading";
+  }
+
+  createReadBtn.addEventListener("click", updateReadStatus);
+  bookCard.appendChild(createReadBtn);
+  createDeleteBtn.setAttribute("class", "bookcard-btn");
+  createDeleteBtn.setAttribute("id", "book"+n);
+  createDeleteBtn.textContent = "Delete Book";
+  createDeleteBtn.addEventListener("click", deleteBook);
+  bookCard.appendChild(createDeleteBtn);
 }
 
-let addBook = new Book("seven ancient wonders", "mathew reilly", 356, true);
+let addBook = new Book("Seven Ancient Wonders", "Mathew Reilly", 365, true);
 myLibrary.push(addBook);
 displayBook(0);
 
@@ -58,6 +95,7 @@ function updateAndDisplayLibrary(){
   myLibrary.forEach(book => displayBook(book));
 }
 
+//remove element from myLibrary based on the id of the button created
 function deleteBook(e){
   let bookId = e.target.id;
   let num = bookId.match(/\d/g);
@@ -67,4 +105,10 @@ function deleteBook(e){
   updateLibrary();
 }
 
-addBookButton.addEventListener("click", addBookToLibrary);
+function addBooktoLibrary1(e){
+  let inputVal =
+  console.log(inputVal);
+}
+
+addBookButton.addEventListener("mouseup", () => document.querySelector(".form-popup").style.display = "grid");
+addBookButton1.addEventListener("mouseup", addBookToLibrary);
